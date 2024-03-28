843b5284aca7f40f2e936beb14d953dd65b93c5f
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-03-28 08:04:00 -0600

    update sam.py

--- sam.py
+++ sam.py
@@ -2,6 +2,11 @@
 
 import  pyfiglet
 import  sys
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+import base64
+
 
 texto = pyfiglet.print_figlet("\n Bunker",  font='slant', colors='blue')
 
@@ -22,3 +27,29 @@ while intentos > 0:
 
 
 
+
+
+key = get_random_bytes(16) # Clave de encriptación
+cipher = AES.new(key, AES.MODE_ECB) # Crea un nuevo objeto cifrado AES
+
+# Contraseña original
+original_password = b'BunkerWallx500'
+
+# Encripta la contraseña original
+encrypted_password = cipher.encrypt(pad(original_password, AES.block_size))
+print("Contraseña encriptada: ", base64.b64encode(encrypted_password))
+
+# Ahora puedes verificar la contraseña ingresada por el usuario
+for _ in range(5):
+    input_password = input("Ingresa tu ID: ").encode()
+
+    # Encripta la contraseña ingresada por el usuario
+    encrypted_input_password = cipher.encrypt(pad(input_password, AES.block_size))
+    
+    if encrypted_input_password == encrypted_password:
+        print("CORRECTO")
+        break
+    else:
+        print("INCORRECTO")
+
+print("Se acabaron tus intentos")
