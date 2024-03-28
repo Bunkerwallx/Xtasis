3c98b923732ab2883d55dd24bfffc4a5a9fe2a27
bunkerwallx <39147256+Bunkerwallx@users.noreply.github.com>
2024-03-28 07:34:58 -0600

    update sam.py

--- sam.py
+++ sam.py
@@ -5,30 +5,16 @@ import  sys
 
 texto = pyfiglet.print_figlet("\n Bunker",  font='slant', colors='blue')
 
-
-def check_password():
-    # Establecer la contraseña correcta y el número de intentos
-    correct_password = "Bunker"
-    attempts = 5
-
-    # Bucle para verificar la contraseña
-    for i in range(attempts):
-        # Solicitar la contraseña al usuario
-        user_password = input("INGRESA TU PASSWORD: ")
-
-        # Comprobar si la contraseña es correcta
-        if user_password == correct_password:
-            print("¡CORRECTO! Has ingresado la contraseña correcta.")
-            break
-        else:
-            print("INCORRECTO. Te quedan {} intentos.".format(attempts - i - 1))
-
-    # Si se agotan todos los intentos, informar al usuario
-    print("Se acabaron tus intentos.")
-
-# Llamar a la función para ejecutarla
-check_password()
-
+intentos =
+while intentos > 0:
+          passwd = input("Ingresa Tu ID>: ")
+     if passwd == pass_co:
+          print('ID correcto')
+    ...:         break
+    ...:     else:
+    ...:         print("ID incorrecto")
+    ...:         intentos -= 1
+    ...:      if intentos == 0:
 
 
 
